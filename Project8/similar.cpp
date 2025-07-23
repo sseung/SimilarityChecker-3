9f49781d1687c231b9c89887651a9aa3fc205e7a
Seungah Lim <sss.lim@samsung.com>
2025-07-23 15:15:30 +0900

    Add TC2 for 0

--- Project8/similar.cpp
+++ Project8/similar.cpp
@@ -3,10 +3,19 @@
 class LengthChecker {
 public:
 	int getLengthScore(std::string str1, std::string str2) {
-		if (str1.length() == str2.length()) {
-			return 60;
+		int str1Len = str1.length();
+		int str2Len = str2.length();
+		if (str1Len == str2Len) {
+			return MAX_LENGTH_SCORE;
+		}
+		if ((str1Len >= str2Len) || (str2Len >= str1Len)) {
+			return MIN_LENGTH_SCORE;
 		}
 	}
+
+private:
+	static const int MAX_LENGTH_SCORE = 60;
+	static const int MIN_LENGTH_SCORE = 0;
 
 };
 
--- Project8/similar_test.cpp
+++ Project8/similar_test.cpp
@@ -1,9 +1,14 @@
 #include "gmock/gmock.h"
 #include "similar.cpp"
 
-
 TEST(TS, TC60) {
 	LengthChecker checker;
 	int result = checker.getLengthScore("ASD", "DSA");
 	EXPECT_EQ(60, result);
+}
+
+TEST(TS, TC0) {
+	LengthChecker checker;
+	int result = checker.getLengthScore("A", "BB");
+	EXPECT_EQ(0, result);
 }
