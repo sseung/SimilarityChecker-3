0e39450783bf6b7c0f5abcaf82bd39ac796b2332
Seungah Lim <sss.lim@samsung.com>
2025-07-23 16:19:24 +0900

    [refactor] Remove vars

--- Project8/similar.cpp
+++ Project8/similar.cpp
@@ -5,18 +5,15 @@
 class LengthChecker {
 public:
 	int getLengthScore(std::string str1, std::string str2) {
-		int str1Len = str1.length();
-		int str2Len = str2.length();
-		if (str1Len == str2Len) {
+		int maxLen = std::max(str1.length(), str2.length());
+		int minLen = std::min(str1.length(), str2.length());
+		if (maxLen == minLen) {
 			return MAX_LENGTH_SCORE;
 		}
-		int maxLen = std::max(str1Len, str2Len);
-		int minLen = std::min(str1Len, str2Len);
 		if (maxLen >= minLen * 2) {
 			return MIN_LENGTH_SCORE;
 		}
-
-		return (MAX_LENGTH_SCORE - (std::abs(str1Len - str2Len) * MAX_LENGTH_SCORE / minLen));
+		return (MAX_LENGTH_SCORE - ((maxLen - minLen) * MAX_LENGTH_SCORE / minLen));
 	}
 
 private:
